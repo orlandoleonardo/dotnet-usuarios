c00d767b38fabd1c5dbe2d149fdbd7105063f9f4
Leonardo <lorlando@est.frba.utn.edu.ar>
2020-12-08 19:59:00 -0300

    first commit

---
+++ Usuarios/Controllers/UsuariosController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Usuarios.Controllers
+{
+    //[Route("[controller]")]
+    public class UsuariosController : Controller
+    {
+        //[Route("[controller]/[action]/{data:int}")]
+        public IActionResult Index(int data)
+        {
+            var url = Url.Action("Metodo", "Usuarios");
+            //return View("Index",data);
+            return Redirect(url);
+        }
+
+        public IActionResult Metodo()
+        {
+            return View();
+        }
+    }
+}
