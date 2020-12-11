b60feaa00e0d4f189bdab19d389d2007871478e2
Leonardo <lorlando@est.frba.utn.edu.ar>
2020-12-11 01:27:24 -0300

    agrego HttpGet con parametros

--- Usuarios/Controllers/UsuariosController.cs
+++ Usuarios/Controllers/UsuariosController.cs
@@ -12,11 +12,13 @@ namespace Usuarios.Controllers
         //[Route("[controller]/[action]/{data:int}")]
         public IActionResult Index(double data)
         {
-            var url = Url.Action("Metodo", "Usuarios", new { edad=23, name="Leo"});
+            //var url = Url.Action("Metodo", "Usuarios", new { edad=23, name="Leo"});
             //return View("Index",data);
+            var url = Url.RouteUrl("Leo");
             return Redirect(url);
         }
 
+        [HttpGet("[Controller]/[action]", Name = "Leo")]
         public IActionResult Metodo(int edad, String name)
         {
             var data = $"Nombre {name} Edad {edad}";
